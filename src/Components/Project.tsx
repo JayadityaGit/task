b7e693ed416b9d1a81176972c61b2c9f7a9a2d16
JAYADITYA <96861162+JayadityaGit@users.noreply.github.com>
2024-01-04 13:28:36 +0530

    Update Project.tsx

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -4,7 +4,7 @@ const Project = () => {
   return (
     <div className=" rounded-lg animate-border  bg-white bg-gradient-to-r from-red-500 via-purple-500 to-blue-500 bg-[length:400%_400%] p-1">
     <div style={{backgroundColor:"#191825"}} className="py-4 rounded-lg flex justify-center md:px-2" >
-      <a href="https://inspiring-creponne-f50ff5.netlify.app/" rel="noreferrer" target="_blank">Live Project ( early stage) !!!</a>
+      <a href="https://joyful-torrone-46c21e.netlify.app/" rel="noreferrer" target="_blank">Live Project ( early stage) !!!</a>
 
     </div>
     </div>
